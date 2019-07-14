f715a3b9f1c3b3f126638243005f7ea5ce8500f4
root <root@master.tasmin.moellenkamp.org>
2019-07-14 20:15:47 +0200

    Basic dynamic inventory, pregenerated

---
+++ dyn_inventory/pregenerated_inventory.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3.5
+
+import os
+import sys
+
+base_inventory=open("/usr/local/securitydevice/server/baseinventory.txt","r")
+base_inventory_content = base_inventory.read()
+print(base_inventory_content)
+
+print("[client_devices]")
+openvpn_statuslog_file = open("/etc/openvpn/openvpn-status.log","r")
+openvpn_statuslog_content = openvpn_statuslog_file.readlines()
+for line in openvpn_statuslog_content:
+	if "10.8." in line:
+		if not "/24" in line:
+			ip=line.split(",")[0]
+			print(ip)
+
