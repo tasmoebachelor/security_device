a779f8f3ad14825fd292f29def34d0f352113de5
root <root@master.tasmin.moellenkamp.org>
2019-07-16 14:00:08 +0200

    more changes

--- scripts/on_configvpn_connect.py
+++ scripts/on_configvpn_connect.py
@@ -12,16 +12,16 @@ import shlex
 import re
 
 # OpenVPN gibt eine reihe von Environmentvariabeln mit. Davon sind interessant:
-# Die IP-Addresse des sich verbindenen Client-Devices im Config-VPN. Dies wird für 
-# für die Ansible-Verbindung zum Client-Device benutzt. 
+# Die IP-Addresse des sich verbindenen Client-Devices im Config-VPN. Dies wird fuer 
+# fuer die Ansible-Verbindung zum Client-Device benutzt. 
 remote_ip = os.environ["ifconfig_pool_remote_ip"]
-# Der Common-Name im für die erfolgreiche Verbinung genutzten Zertifikat
+# Der Common-Name im fuer die erfolgreiche Verbinung genutzten Zertifikat
 common_name = os.environ["common_name"]
 # Wo man schon mal im Python ist, kann man auch gleich die Client-ID rausschneiden.
 client_id =  re.search("clnt(.*)dev",common_name).group(1)
 
-# Das Ansible-Script muss hier sudo nutzen, da der OpenVPN-Server fürs Config-VPN unter 
-# dem User openvpn läuft, das Ansible-Playbook braucht aber root.
+# Das Ansible-Script muss hier sudo nutzen, da der OpenVPN-Server fuers Config-VPN unter 
+# dem User openvpn laeuft, das Ansible-Playbook braucht aber root.
 cmd="sudo -b ansible-playbook -i centctrl," + remote_ip +" /usr/local/securitydevice/ansible/playbook_onconfigvpnconnect.yaml --extra-vars=\"remote_ip="+remote_ip+" common_name="+ common_name +" client_id="+ client_id+"\" " 
 args = shlex.split(cmd)
 subprocess.call(args)
